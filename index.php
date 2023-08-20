012f777d4918f44ba0f20c81f9e0a04e2cbf1ae6
LuizDevSpace <luizgreco777@gmail.com>
2023-08-20 18:08:54 -0300

    commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include "Model.php";
 $model = new Model();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
